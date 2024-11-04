e1d0ea5dd463be5609a22e46248ef56c7e888ab0
Ivan Abramovich <ivan.abramovich@eltex.loc>
2024-11-04 17:36:42 +0700

    Рефакторинг моделей

--- app/database/database.go
+++ app/database/database.go
@@ -3,74 +3,13 @@ package database
 import (
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/IvanAbramovichWork/family-inventory-app/app/config"
+	"github.com/IvanAbramovichWork/family-inventory-app/app/models"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
-
-type Family struct {
-	Id        int       `db:"id"`
-	Name      string    `db:"name"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
-}
-
-type User struct {
-	Id       int    `db:"id"`
-	Name     string `db:"name"`
-	Email    string `db:"email"`
-	Password string `db:"password"`
-	Role     string `db:"role"`
-}
-
-type FamilyMember struct {
-	Id       int    `db:"id"`
-	FamilyId int    `db:"family_id"`
-	UserId   int    `db:"user_id"`
-	Role     string `db:"role"`
-}
-
-type Product struct {
-	Id          int       `db:"id"`
-	Name        string    `db:"name"`
-	Category    string    `db:"category"`
-	Description string    `db:"description"`
-	Photo       string    `db:"photo_url"`
-	Barcode     string    `db:"barcode"`
-	CreatedAt   time.Time `db:"created_at"`
-	UpdatedAt   time.Time `db:"updated_at"`
-}
-
-type Inventory struct {
-	Id             int       `db:"id"`
-	FamilyId       int       `db:"family_id"`
-	ProductId      int       `db:"product_id"`
-	Quantity       int       `db:"quantity"`
-	ExpirationDate time.Time `db:"expiration_date"`
-	CreatedAt      time.Time `db:"created_at"`
-	UpdatedAt      time.Time `db:"updated_at"`
-}
-type Transaction struct {
-	Id        int       `db:"id"`
-	FamilyId  int       `db:"family_id"`
-	ProductId int       `db:"product_id"`
-	UserId    int       `db:"user_id"`
-	Action    string    `db:"action"`
-	Quantity  int       `db:"quantity"`
-	CreatedAt time.Time `db:"created_at"`
-}
-
-type Notification struct {
-	Id        int       `db:"id"`
-	UserId    int       `db:"user_id"`
-	ProductId int       `db:"product_id"`
-	Message   string    `db:"message"`
-	SentAt    time.Time `db:"sent_at"`
-	IsRead    bool      `db:"is_read"`
-}
 
 func InitDB(cfg *config.Config) *sqlx.DB {
 	var db *sqlx.DB
@@ -87,7 +26,7 @@ func InitDB(cfg *config.Config) *sqlx.DB {
 	return db
 }
 
-func createFamily(db *sqlx.DB, family *Family) error {
+func createFamily(db *sqlx.DB, family *models.Family) error {
 	_, err := db.NamedExec(`INSERT INTO family (name, created_at, updated_at) VALUES (:name, :created_at, :updated_at)`, map[string]interface{}{
 		"name":       family.Name,
 		"created_at": family.CreatedAt,
@@ -102,7 +41,7 @@ func createFamily(db *sqlx.DB, family *Family) error {
 	return nil
 }
 
-func createFamilyMember(db *sqlx.DB, familyMember *FamilyMember) error {
+func createFamilyMember(db *sqlx.DB, familyMember *models.FamilyMember) error {
 	_, err := db.NamedExec(`INSERT INTO family_members (family_id, user_id, role) VALUES (:family_id, :user_id, :role)`, map[string]interface{}{
 		"family_id": familyMember.FamilyId,
 		"user_id":   familyMember.UserId,
@@ -117,7 +56,7 @@ func createFamilyMember(db *sqlx.DB, familyMember *FamilyMember) error {
 	return nil
 }
 
-func insertInventory(db *sqlx.DB, inventory *Inventory) error {
+func insertInventory(db *sqlx.DB, inventory *models.Inventory) error {
 	_, err := db.NamedExec(`INSERT INTO inventory (family_id, product_id, quantity, expiration_date, created_at, updated_at) VALUES (:family_id, :product_id, :quantity, :expiration_date, :created_at, :updated_at)`, map[string]interface{}{
 		"family_id":       inventory.FamilyId,
 		"product_id":      inventory.ProductId,
@@ -136,7 +75,7 @@ func insertInventory(db *sqlx.DB, inventory *Inventory) error {
 	return nil
 }
 
-func createProduct(db *sqlx.DB, product *Product) error {
+func createProduct(db *sqlx.DB, product *models.Product) error {
 	_, err := db.NamedExec(`INSERT INTO products (name, category, description, photo_url, barcode, created_at, updated_at) VALUES (:name, :category, :description, :photo_url, :barcode, :created_at, :updated_at)`, map[string]interface{}{
 		"name":        product.Name,
 		"category":    product.Category,
@@ -156,7 +95,7 @@ func createProduct(db *sqlx.DB, product *Product) error {
 	return nil
 }
 
-func createUser(db *sqlx.DB, user User) error {
+func createUser(db *sqlx.DB, user models.User) error {
 	query := `INSERT INTO users (name, email, password_hash, role) VALUES (:name, :email, :password, :role)`
 	_, err := db.NamedExec(query, user)
 
@@ -168,8 +107,8 @@ func createUser(db *sqlx.DB, user User) error {
 	return nil
 }
 
-func getUserByID(db *sqlx.DB, userID int) (*User, error) {
-	var user User
+func getUserByID(db *sqlx.DB, userID int) (*models.User, error) {
+	var user models.User
 	query := `SELECT * FROM users WHERE id = $1`
 	err := db.Get(&user, query, userID)
 	return &user, err
